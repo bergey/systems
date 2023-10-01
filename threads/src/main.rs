a16ef8fd5f8f55159d1631287cf7b2bf30c9841e
Daniel Bergey <daniel.bergey@braze.com>
2023-10-01 13:24:00 -0400

    two threads, write to file

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::thread;
+use std::fs::OpenOptions;
+use std::io::{Seek, SeekFrom, Write};
+
+const FILENAME: &str = "access_counts";
+
+fn main() -> Result<(), std::io::Error> {
+    let mut file = OpenOptions::new().write(true).create(true).truncate(true).open(FILENAME)?;
+
+    let other_thread = thread::spawn(|| {
+        // this one does not create nor truncate
+        let mut file = OpenOptions::new().write(true).open(FILENAME)?;
+        let doc = [1; 1024];
+        file.write_all(&doc)?;
+        Ok::<(), std::io::Error>(())
+    });
+
+    file.seek(SeekFrom::Start(1024))?;
+    let doc = [2; 1024];
+    file.write_all(&doc)?;
+
+    match other_thread.join() {
+        Err(_) => println!("other thread panic"),
+        Ok(Ok(())) => (),
+        Ok(_) => println!("other thread Error")
+    }
+    Ok(())
 }
